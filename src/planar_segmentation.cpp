f404009d39d35f3c92aa453b3fb3c64b54f3244f
Adit Namdev <aditn4@gmail.com>
2017-04-12 11:36:52 -0400

    nearly finished, still memory corruption error with the mug

--- src/planar_segmentation.cpp
+++ src/planar_segmentation.cpp
@@ -8,14 +8,48 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/filters/project_inliers.h>
 
+#include <string.h>
+#include <stdio.h>
+
 #include <iostream>
 
+void savePCD(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud,
+             char* fileName,
+             bool objectOrProj){
 
-void planeSeg(pcl::SACSegmentation<pcl::PointXYZRGB> segmentation,
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,pcl::ModelCoefficients::Ptr coefficients,pcl::PointIndices::Ptr
-planeIndices)
+  char *name = new char[80];
+  strcpy(name, "");
+  // Boolean used for determining if point cloud is of object or of
+  // object's projection.
+  if (objectOrProj) {
+    strcat(name,fileName);
+    strcat(name,"_obj.pcd");
+  }
+  else{
+    strcat(name,fileName);
+    strcat(name,"_proj.pcd");
+  }
+  printf("trying to saving point cloud to %s\n",name);
+  pcl::io::savePCDFileBinary (name, *pointCloud);
+  printf("saving point cloud to %s\n",name);
+
+}
+       
+
+void viewPointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud){
+  pcl::visualization::CloudViewer pclViewer("Objects on table");
+  pclViewer.showCloud(pointCloud);
+  while (!pclViewer.wasStopped())
+  {
+      // Do nothing but wait.
+  }
+}
+
+void planeSeg(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+              pcl::ModelCoefficients::Ptr coefficients,
+              pcl::PointIndices::Ptr planeIndices)
 {
-  // Get the plane model, if present.
+  pcl::SACSegmentation<pcl::PointXYZRGB> segmentation;
   segmentation.setInputCloud(cloud);
   segmentation.setModelType(pcl::SACMODEL_PLANE);
   segmentation.setMethodType(pcl::SAC_RANSAC);
@@ -25,115 +59,107 @@ planeIndices)
   printf("num_points: %d\n",planeIndices->indices.size());
 }
 
+void projectPoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr proj_cloud,
+                   pcl::ModelCoefficients::Ptr coefficients,
+                   pcl::PointCloud<pcl::PointXYZRGB>::Ptr plane)
+{
+  pcl::ProjectInliers<pcl::PointXYZRGB> proj;
+  proj.setModelType (pcl::SACMODEL_PLANE);
+  proj.setInputCloud (plane);
+  proj.setModelCoefficients (coefficients);
+  proj.filter (*proj_cloud);
+}
+
+void removePlane(pcl::PointCloud<pcl::PointXYZRGB>::Ptr outCloud,
+                 pcl::PointCloud<pcl::PointXYZRGB>::Ptr inputCloud,
+                 pcl::PointIndices::Ptr cloudIndices,
+                 bool setNeg)
+{
+  pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+  if (setNeg) extract.setNegative(true);
+  else extract.setNegative(false);
+  extract.setInputCloud(inputCloud);
+  extract.setIndices(cloudIndices);
+  extract.filter(*outCloud);
+}
+
+
 int main(int argc, char** argv)
 {
   /* Objects for storing point clouds at different stages */
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr init_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr plane(new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr convexHull(new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr objects(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZRGB>);
 
   /* Read in PCD file */
-  if (pcl::io::loadPCDFile<pcl::PointXYZRGB>(argv[1], *cloud) != 0)
+  if (pcl::io::loadPCDFile<pcl::PointXYZRGB>(argv[1], *init_cloud) != 0)
   {
     return -1;
   }
 
   /* Perform plane segmentation */
-  pcl::SACSegmentation<pcl::PointXYZRGB> segmentation;
-  // stores plane function
-  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+  pcl::ModelCoefficients::Ptr plane_coef(new pcl::ModelCoefficients);
   pcl::PointIndices::Ptr planeIndices(new pcl::PointIndices);
-  planeSeg(segmentation,cloud,coefficients,planeIndices);
-  
-  // Print plane coefficients: ax+by+cz+d=0
-  /*for (int i=0;i<4;i++)
-  {
-    printf("coef[%d]:%f,",i,coefficients->values[i]);
-  }*/
-  //printf("\n");
+  planeSeg(init_cloud,plane_coef,planeIndices);
 
-  pcl::visualization::CloudViewer viewerObjects("Objects on table");
   if (planeIndices->indices.size() == 0)
+  {
     std::cout << "Could not find a plane in the scene." << std::endl;
+    return -1;
+  }
   else
   {
     // Copy the points of the plane to a new cloud.
-    pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-    //extract.setNegative(true);
-    extract.setInputCloud(cloud);
-    extract.setIndices(planeIndices);
-    extract.filter(*plane);
-    /*viewerObjects.showCloud(plane);
-    while (!viewerObjects.wasStopped())
-    {
-        // Do nothing but wait.
-    }*/
-  
+    removePlane(plane,init_cloud,planeIndices,false);
+
     // Retrieve the convex hull.
     pcl::ConvexHull<pcl::PointXYZRGB> hull;
     hull.setInputCloud(plane);
+    
     // Make sure that the resulting hull is bidimensional.
     hull.setDimension(2);
     hull.reconstruct(*convexHull);
 
-    // Redundant check.*/
-    if (hull.getDimension() == 2)
-    {
-      // Prism object.
-      pcl::ExtractPolygonalPrismData<pcl::PointXYZRGB> prism;
-      prism.setInputCloud(cloud);
-      prism.setInputPlanarHull(convexHull);
-      // First parameter: minimum Z value. Set to 0, segments objects lying on the plane (can be negative).
-      // Second parameter: maximum Z value, set to 10cm. Tune it according to the height of the objects you expect.
-      prism.setHeightLimits(0.0f, 0.3f);
-      pcl::PointIndices::Ptr objectIndices(new pcl::PointIndices);
+    // Prism object.
+    pcl::ExtractPolygonalPrismData<pcl::PointXYZRGB> prism;
+    prism.setInputCloud(init_cloud);
+    prism.setInputPlanarHull(convexHull);
 
-      prism.segment(*objectIndices);
+    // First parameter: minimum Z value. Set to 0, segments objects lying on the plane (can be negative).
+    // Second parameter: maximum Z value. Tune it to the expected height of the object.
+    prism.setHeightLimits(0.0f, 0.3f);
+    pcl::PointIndices::Ptr objectIndices(new pcl::PointIndices);
 
-      // Get and show all points retrieved by the hull.
-      //pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-      extract.setIndices(objectIndices);
-      extract.filter(*objects);
-      
-    }
-    else std::cout << "The chosen hull is not planar." << std::endl;
-  }    
+    prism.segment(*objectIndices);
+
+    // Get points retrieved by the hull.
+    removePlane(objects,init_cloud,objectIndices,false);
+  }
     
   // Perform second planar segmentation
   // TODO: WHY DO I NEED TO DO PLANE SEGMENTATION AGAIN
-  planeSeg(segmentation,objects,coefficients,planeIndices);
-   
+  planeSeg(objects,plane_coef,planeIndices);
+  
   /* Remove Floor Plane from Point Cloud */
-  pcl::ExtractIndices<pcl::PointXYZRGB> extract2;
-  extract2.setNegative(true);
-  extract2.setInputCloud(objects);
-  extract2.setIndices(planeIndices);
-  extract2.filter(*plane);
+  removePlane(plane, objects, planeIndices, true);
 
-  //pcl::visualization::CloudViewer viewerObjects("Objects on table");
-  /*viewerObjects.showCloud(plane);
-  while (!viewerObjects.wasStopped())
-  {
-    // Do nothing but wait.
-  }*/
-  
-  /* Two Options for Projecting onto Floor Plane:
-     1: Transform to (0,0,0) orientation, then set Z coordinate to 0
-     2. Project to Floor Plane in Place */
-  /* Option 1 */
+  /* Save point cloud of object */
+  savePCD(plane,argv[1],true);
+  //viewPointCloud(plane);
 
-  /* Option 2 */
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZRGB>);
-  pcl::ProjectInliers<pcl::PointXYZRGB> proj;
-  proj.setModelType (pcl::SACMODEL_PLANE);
-  proj.setInputCloud (plane);
-  proj.setModelCoefficients (coefficients);
-  proj.filter (*cloud_projected);
+  /* Project Object Points to Tabletop plane 2 */
+  projectPoints(cloud_projected, plane_coef, plane);
   
-  viewerObjects.showCloud(cloud_projected);
-  while (!viewerObjects.wasStopped())
-  {
-    // Do nothing but wait.
-  }
+  /* Save point cloud of projection */
+  savePCD(cloud_projected,argv[1],false);
+  //viewPointCloud(cloud_projected);
+  
+
+  /*init_cloud.clear();
+  *plane.clear();
+  *convexHull.clear();
+  *objects.clear();
+  *cloud_projected.clear();*/
 }
